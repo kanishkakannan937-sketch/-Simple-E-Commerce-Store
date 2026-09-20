e4b545d55c1f8ab897e011901daa8244f1e2d8a4
Kanishka K <kanishkakannan937@gmail.com>
2026-09-20 10:26:17 +0530

    Fix product loading

--- js/product.js
+++ js/product.js
@@ -4,7 +4,7 @@ let allProducts = [];
 
 async function loadProducts() {
     try {
-        const response = await fetch(API_URL);
+        const response = await fetch("http://localhost:5000/api/products");
 
         const data = await response.json();
 
